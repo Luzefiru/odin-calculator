21aa2c35763bcdb65151874aae6332778089e941
Luzefiru <ian.deejay.iv@gmail.com>
2023-02-03 14:57:05 +0800

    feat: add subtract() function

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
 function add(a,b) {
     return Number(a) + Number(b);
 }
+
+/**
+ * Subtracts {b} from {a} and returns the result.
+ * Can accept 'string' or 'number' inputs.
+ */
+function subtract(a,b)
+{
+    return Number(a) - Number(b);
+}
